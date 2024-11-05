c8a5668bb4a5b49ac7905113c38a18c9247b823f
thao-nguyen-10 <quynh.nguyen.thao123@gmail.com>
2024-11-05 13:18:07 +0700

    Update map_display.py

--- src/map_display.py
+++ src/map_display.py
@@ -25,8 +25,18 @@ def create_map():
     return folium_map
 
 
-def add_marker(
-    folium_map, location, popup_text="Marker", icon=folium.Icon(color=MARKER_COLOR)
-):
-    folium.Marker(location, popup=popup_text, icon=icon).add_to(folium_map)
-    return folium_map
+# def add_marker(
+#    folium_map, location, popup_text="Marker", icon=folium.Icon(color=MARKER_COLOR)
+#):
+#    folium.Marker(location, popup=popup_text, icon=icon).add_to(folium_map)
+#    return folium_map
+
+# Function to add a marker on the map
+def add_marker(map_obj, location):
+    folium.Marker(location).add_to(map_obj)
+
+# Create initial map for start and end points
+def create_map_with_marker(initial_coords):
+    m = folium.Map(location=initial_coords, zoom_start=12)
+    add_marker(m, initial_coords)
+    return m
